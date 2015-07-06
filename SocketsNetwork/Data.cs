0ce9eba188e4a2949ec189f45b6f4b1485723946
Sune <sunenilausen@gmail.com>
2015-07-06 17:25:48 +0200

    combined command and function enums and trimmed the data object to be more efficient

--- SocketsNetwork/Data.cs
+++ SocketsNetwork/Data.cs
@@ -12,72 +12,51 @@ using System.Linq;
 public class Data {
 
     public string strName;      //Name by which the client logs into the room
-    public string strMessage;   //Message text
-    public Command cmdCommand;  //Command type (login, logout, send message, etcetera)
     public Function function;  //Functionname
     public object[] objects; //Function Parameters
 
     //Default constructor
     public Data()
     {
-        this.cmdCommand = Command.Null;
-        this.function = Function.Null;
-        this.strMessage = null;
         this.strName = null;
+        this.function = Function.Null;
         this.objects = null;
     }
 
     //Converts the bytes into an object of type Data
     public Data(byte[] data)
     {
-        //The first four bytes are for the Command
-        this.cmdCommand = (Command)BitConverter.ToInt32(data, 0);
 
-        //The next four store the length of the name
-        int nameLen = BitConverter.ToInt32(data, 4);
+        //The first four bytes store the length of the name
+        int nameLen = BitConverter.ToInt32(data, 0);
 
         //This check makes sure that strName has been passed in the array of bytes
         if (nameLen > 0)
-            this.strName = Encoding.UTF8.GetString(data, 8, nameLen);
+            this.strName = Encoding.UTF8.GetString(data, 4, nameLen);
         else
             this.strName = null;
 
-        if (this.cmdCommand == Command.Function)
-        {
-            //The next four store the function
-            this.function = (Function)BitConverter.ToInt32(data, 8 + nameLen);
+        //The next four store the function
+        this.function = (Function)BitConverter.ToInt32(data, 4 + nameLen);
 
-            //The rest store the dynamically stored parameters
-            byte[] objectData = new byte[1024];
+        
+        //The rest store the dynamically stored parameters
+        byte[] objectData = new byte[1024];
 
-            //I am making a new array with only the parameters, could probably find a better way
-            Array.Copy(data, 12 + nameLen, objectData, 0, 512);
+        //I am making a new array with only the parameters, could probably find a better way
+        Array.Copy(data, 8 + nameLen, objectData, 0, 512);
 
-            BinaryFormatter formatter = new BinaryFormatter();
-            MemoryStream ms = new MemoryStream(objectData);
-            
-            this.objects = (object[])formatter.Deserialize(ms);
-        }
-        else
-        {
-            //The next four store the length of the message
-            int msgLen = BitConverter.ToInt32(data, 8 + nameLen);
+        BinaryFormatter formatter = new BinaryFormatter();
+        MemoryStream ms = new MemoryStream(objectData);
 
-            //This checks for a null message field
-            if (msgLen > 0)
-                this.strMessage = Encoding.UTF8.GetString(data, 12 + nameLen, msgLen);
-            else
-                this.strMessage = null;
-        }
+        this.objects = (object[])formatter.Deserialize(ms);
+
     }
 
     //Converts the Data structure into an array of bytes
     public byte[] ToByte()
     {
         List<byte> result = new List<byte>();
-
-        //First four are for the Command
-        result.AddRange(BitConverter.GetBytes((int)cmdCommand));
 
         //Add the length of the name
         if (strName != null)
@@ -89,35 +68,20 @@ public class Data {
         if (strName != null)
             result.AddRange(Encoding.UTF8.GetBytes(strName));
 
-        if (cmdCommand == Command.Function)
+        
+        result.AddRange(BitConverter.GetBytes((int)function));
+
+        //Convert each parameter(objects) to byte array
+        if (objects != null)
         {
-            result.AddRange(BitConverter.GetBytes((int)function));
-
-            //Convert each parameter(objects) to byte array
-            if (objects != null)
-            {
-                BinaryFormatter formatter = new BinaryFormatter();
-                MemoryStream ms = new MemoryStream();
-                formatter.Serialize(ms, objects);
-                result.AddRange(ms.ToArray());
-            }
-            else 
-                result.AddRange(BitConverter.GetBytes(0));
-
+            BinaryFormatter formatter = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            formatter.Serialize(ms, objects);
+            result.AddRange(ms.ToArray());
         }
-        else
-        {
-            //Length of the message
-            if (strMessage != null)
-                result.AddRange(BitConverter.GetBytes(strMessage.Length));
-            else
-                result.AddRange(BitConverter.GetBytes(0));
-
-            //Add the message text to our array of bytes
-            if (strMessage != null)
-                result.AddRange(Encoding.UTF8.GetBytes(strMessage));
-        }
-
+        else 
+            result.AddRange(BitConverter.GetBytes(0));
+       
         return result.ToArray();
     }
 }
--- SocketsNetwork/EnumFunction.cs
+++ SocketsNetwork/EnumFunction.cs
@@ -4,19 +4,10 @@ public enum Function
     Null,        //No command
     Login,      //Log into the server
     Logout,     //Logout of the server
-    Message,    //Send a text message to all the chat clients
     List,       //Get a list of users in the chat room from the server
 
     InitSuccess,
-    LoadGame,
-    EveryoneRaiseHand,
-    RaiseHand,
-    IntFunction,
-    StringFunction,
-    ObjectParamsFunction,
-    CountListFunction,
-    DisplayCount,
-    StringMessage,
-    StartTime
+    DebugMessage,
+    TextboxMessage
 }
 
